bd1253cc2ce7381a94fbc3b414136c0602bc7b16
edu-as <edu-as@hotmail.com>
2022-01-12 18:40:10 -0300

    adding styles with Material-ui

--- src/components/register-form/register-form.jsx
+++ src/components/register-form/register-form.jsx
@@ -1,9 +1,26 @@
-import { React } from 'react';
+import * as React from 'react';
+import Button from '@mui/material/Button';
 
 function RegisterForm(){
     return(
         <form>
-            <input type="text"/>
+            <label>Nome</label>
+            <input type="text" />
+            
+            <label>Sobrenome</label>
+            <input type="text" />
+
+            <label>CPF</label>
+            <input type="text" />
+
+            <label>Promoções</label>
+            <input type="checkbox" />
+
+            <label>Novidades</label>
+            <input type="checkbox" />
+            <Button type="submit" variant="outlined" color="success">
+                Cadastrar
+            </Button>
         </form>
     );
 }
