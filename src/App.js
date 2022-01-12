fb613e88d72d7618c7a5aa21d519eb3ce1ffdbb4
edu-as <edu-as@hotmail.com>
2022-01-12 03:33:24 -0300

    adding form component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import React, { Component } from 'react';
+import { Fragment } from 'react/cjs/react.production.min';
 import './App.css';
+import RegisterForm from './components/register-form/register-form';
 
-function App() {
-  return (
-   <div>
-    first test
-   </div>
-  );
+class App extends Component {
+  render() {
+    return(
+      <Fragment>
+        <h1>Formulário de cadastro</h1>
+        <RegisterForm/>
+      </Fragment>
+    );
+  }
 }
 
 export default App;
