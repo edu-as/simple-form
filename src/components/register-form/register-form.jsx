9c95c12fcde49a70600f62ab9234adc903a81cdb
edu-as <edu-as@hotmail.com>
2022-01-13 15:23:40 -0300

    stylized form0

--- src/components/register-form/register-form.jsx
+++ src/components/register-form/register-form.jsx
@@ -1,22 +1,45 @@
 import * as React from 'react';
-import Button from '@mui/material/Button';
-import TextField from "@material-ui/core/TextField";
+import { Button, TextField, Switch, FormControlLabel, SendIcon} from '@material-ui/core';
+
 function RegisterForm(){
-    return(
-        <form>
-            <TextField id="nome" label="Nome" variant="outlined" margin="normal" fullWidth />
-            <TextField id="nome" label="Sobrenome" variant="outlined" margin="normal" fullWidth />
-            <TextField id="nome" label="CPF" variant="outlined" margin="normal" fullWidth/>
-
-            <label>Promoções</label>
-            <input type="checkbox" />
-
-            <label>Novidades</label>
-            <input type="checkbox" />
-            <Button type="submit" variant="outlined" color="success">
-                Cadastrar
-            </Button>
-        </form>
+  return(
+    <form>
+        <TextField
+            id="nome"
+            label="Nome"
+            variant="outlined"
+            margin="normal"
+            fullWidth 
+        />
+        <TextField
+            id="nome"
+            label="Sobrenome"
+            variant="outlined"
+            margin="normal"
+            fullWidth 
+        />
+        <TextField
+            id="nome"
+            label="CPF"
+            variant="outlined"
+            margin="normal"
+            fullWidth 
+        />
+        
+        <FormControlLabel
+            label="Promoções"
+            control={<Switch name="novidades" defaultChecked color="primary" />}
+        />
+        <FormControlLabel
+            label="Novidades"
+            control={<Switch name="promocoes" defaultChecked color="secondary" />}
+        />
+        
+        <Button 
+            type="submit"variant="outlined" color="success" >
+            Cadastrar
+        </Button>
+    </form>
     );
 }
 
